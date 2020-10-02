600c6304c0de4cdcaf24d4bb4483b33f1b6d2ed8
unknown <Bhavit Kanthalia>
2020-10-02 15:22:25 +0530

    2D-Array

---
+++ cpp/2D-Array.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the hourglassSum function below.
+int hourglassSum(vector<vector<int>> arr) {
+    
+int max = INT_MIN ;
+vector<vector<int>> sum(4,vector<int>(4,0));
+for (int i=0; i<=3; i++)
+{
+for (int j=0; j<=3; j++)
+    {
+        sum[i][j]= arr[i][j]+sum[i][j]+arr[i][j+1]+arr[i][j+2];
+        sum[i][j]= arr[i+1][j+1]+sum[i][j];
+        sum[i][j]= arr[i+2][j]+sum[i][j]+arr[i+2][j+1]+arr[i+2][j+2];
+
+    }
+}
+for (int i=0; i<=3; i++)
+{
+for (int j=0; j<=3; j++)
+    {
+        if(sum[i][j]>=max)
+        {
+            max = sum[i][j];
+        }
+
+    }
+
+}
+
+return max;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    vector<vector<int>> arr(6);
+    for (int i = 0; i < 6; i++) {
+        arr[i].resize(6);
+
+        for (int j = 0; j < 6; j++) {
+            cin >> arr[i][j];
+        }
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    int result = hourglassSum(arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
