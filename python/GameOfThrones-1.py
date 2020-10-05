d5747d6c113f002cb735517915dc05b54b834b33
unknown <Bhavit Kanthalia>
2020-10-05 21:52:06 +0530

    GameOfThronesw.py

---
+++ python/GameOfThrones-1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/py
+if __name__ == '__main__':
+    string = input()
+    found = True
+    # Write the code to find the required palindrome and then assign the variable 'found' a value of True or False
+    oddCnt = 0
+    letterCnt = [0] * 26
+     
+    for letter in string:
+        letterCnt[ord(letter)-ord('a')] += 1
+     
+    for cnt in letterCnt:
+        oddCnt += cnt % 2
+         
+    if oddCnt > 1:
+        found = False
+ 
+    if not found:
+        print("NO")
+    else:
+        print("YES")
